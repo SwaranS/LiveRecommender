cc1302b095b8f000b47cb9d23ac72367f3cf8daa
swaran <singhsw@tcd.ie>
2017-01-11 23:54:02 +0000

    Started Lucane Keyword Extraction

--- src/main/java/com/recommender/extractor/Main.java
+++ src/main/java/com/recommender/extractor/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
         KeywordsExtractor ks = new KeywordsExtractor();
         try {
-            ks.guessFromString("input");
+            System.out.println(ks.guessFromString("input").get(0).getTerms());
         } catch (IOException e) {
             e.printStackTrace();
         }
